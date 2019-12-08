13401ed9806d79f8cb74f1ac0de38e68ead02e7a
Terada Masaru <mterada1228@gmail.com>
2019-12-08 16:48:49 +0900

    Initial Commit

---
+++ LineCommon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LineCommon
+//
+//  Created by 寺田優 on 2019/12/08.
+//  Copyright © 2019 寺田優. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
